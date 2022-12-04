96ca915d89700095e916076cbe5604549b15b41e
Khaled Gamal <khaled.gamal@b5digital.dk>
2022-12-04 03:22:30 +0200

    updating readme file steps to add new provider

--- source/afterInstall.sh
+++ source/afterInstall.sh
@@ -1,6 +1,8 @@
 # Fix Storage Permessions
+mv .env.example .env
 chmod 755 -R storage/logs/
 chmod 777 -R storage/framework/sessions/
+sudo chmod o+w ./storage/ -R
 chmod 777 -R storage/framework/views/
 chmod 777 -R storage/framework/cache/
 chmod 777 -R bootstrap/cache/
